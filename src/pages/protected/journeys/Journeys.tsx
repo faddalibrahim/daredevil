ed8b93d1349e38058746ce2c8ca8e14ecbfc8c2c
Simon-157 <boatengsimonjnr157@gmail.com>
2023-06-13 16:32:24 +0000

    feat:journeys page info simulation done

--- src/pages/protected/journeys/Journeys.tsx
+++ src/pages/protected/journeys/Journeys.tsx
@@ -1,5 +1,51 @@
-const Journeys = () => {
-  return <div>Journeys</div>;
+import React from "react";
+import { User, Journey } from "./types";
+import JourneysStyles from "./Journeys.module.css";
+import useAuth from "@hooks/useAuth";
+
+type JourneysProps = {
+};
+
+const Journeys: React.FC<JourneysProps> = () => {
+  const { auth } = useAuth()
+  // Compute the number of completed and abandoned journeys
+  const completedJourneys = auth.journeys.filter(
+    (journey: Journey) => journey.milestone === "completed"
+  );
+  const abandonedJourneys = auth.journeys.filter(
+    (journey: Journey) => journey.milestone === "abandoned"
+  );
+
+  return (
+    <div className={JourneysStyles.container}>
+      <div className={JourneysStyles.completed}>
+        <div className={JourneysStyles.metric}>
+          <div className={JourneysStyles.metricValue}>{completedJourneys.length}/{auth.journeys.length}</div>
+          <div className={JourneysStyles.metricLabel}>Journeys Completed</div>
+        </div>
+      </div>
+      <div className={JourneysStyles.abandoned}>
+        <div className={JourneysStyles.metric}>
+          <div className={JourneysStyles.metricValue}>{abandonedJourneys.length}/{auth.journeys.length}</div>
+          <div className={JourneysStyles.metricLabel}>Journeys Abandoned</div>
+        </div>
+      </div>
+      <div className={JourneysStyles.journeyList}>
+        {auth.journeys.map((journey: Journey) => (
+          <div
+            className={JourneysStyles.journey}
+            key={journey.id}
+            onClick={() => {
+              // Handle journey click event
+              console.log(`Journey ${journey.id} clicked`);
+            }}
+          >
+            {journey.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Journeys;
