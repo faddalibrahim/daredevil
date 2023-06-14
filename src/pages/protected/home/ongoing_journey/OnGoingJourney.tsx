3e44cb4d44072339804ceda47c94fef29a1c066f
Faddal Ibrahim <faddalibrahim@gmail.com>
2023-06-14 10:04:41 +0000

    feat: circular progress bar created

--- src/pages/protected/home/ongoing_journey/OnGoingJourney.tsx
+++ src/pages/protected/home/ongoing_journey/OnGoingJourney.tsx
@@ -4,16 +4,32 @@ import darkClock from "@assets/images/dark-clock.png";
 import Button from "@components/button/Button";
 import { BulbIcon } from "@assets/icons/Icons";
 import { CorrectIcon, SwapIcon, AbortIcon } from "@assets/icons/Icons";
+import CircularProgressBar from "@components/progress_bar/circular_progress_bar/CircularProgressBar";
 const OnGoingJourney = () => {
   return (
     <div className={OngoingJourneyStyles.wrapper}>
       <div className={OngoingJourneyStyles.mission_announcement}>
         <img src={darkBell} alt="" />
-        <h1>Today's Dare</h1>
+        <h1>Today's Mission</h1>
       </div>
       <div className={OngoingJourneyStyles.mission_display}>
-        <h2>Ask your crush out</h2>
-        <div className={OngoingJourneyStyles.stats}>statistics</div>
+        <div className={OngoingJourneyStyles.mission_content}>
+          <h1>Ask your crush out</h1>
+          <Button
+            style={{
+              backgroundColor: "rgb(255 255 255 / 0.1)",
+              border: "none",
+              color: "var(--app-white)",
+            }}
+          >
+            description
+          </Button>
+        </div>
+        <div className={OngoingJourneyStyles.mission_stats}>
+          <CircularProgressBar total={28} chunk={7} label="completed" />
+          <CircularProgressBar total={3} chunk={3} label="swaps" />
+          <CircularProgressBar total={3} chunk={1} label="aborted" />
+        </div>
       </div>
       <div className={OngoingJourneyStyles.timer_action_buttons}>
         <img src={darkClock} alt={darkClock} />
