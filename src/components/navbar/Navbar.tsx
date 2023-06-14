f9a91e4cab689a43fadb0858d36b727e50165efa
Faddal Ibrahim <faddalibrahim@gmail.com>
2023-06-14 21:07:06 +0000

    feat: bottom nav on phone screen shows more icon for settings and logout buttons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 // libraries
 import { NavLink } from "react-router-dom";
+import { MouseEvent, useState } from "react";
 
 //components
 import { Logo } from "@assets/icons/Icons";
@@ -10,6 +11,12 @@ import NavbarStyles from "./Navbar.module.css";
 //utils
 import { PAGES, EXTRA_MENU } from "./pages";
 const Navbar = () => {
+  const [toggleClass, setToggleClass] = useState("extra__menu");
+  const toggleExtraMenu = () => {
+    toggleClass === "extra__menu"
+      ? setToggleClass("extra__menu__float")
+      : setToggleClass("extra__menu");
+  };
   return (
     <nav className={NavbarStyles.navbar}>
       <div className={NavbarStyles.navbar__logo}>
@@ -18,18 +25,35 @@ const Navbar = () => {
       <div className={NavbarStyles.navbar__menu}>
         {PAGES.map((page, index) => (
           <div className={NavbarStyles.navbar__menu__item} key={index}>
-            <NavLink to={page.path}>
-              <div className={NavbarStyles.navbar__menu__item__icon}>
-                {page.icon}
-              </div>
-              <small className={NavbarStyles.navbar__menu__item__text}>
-                {page.name}
-              </small>
-            </NavLink>
+            {index == PAGES.length - 1 ? (
+              <NavLink
+                to={page.path}
+                onClick={(e: MouseEvent<HTMLElement>) => {
+                  e.preventDefault();
+                  toggleExtraMenu();
+                }}
+              >
+                <div className={NavbarStyles.navbar__menu__item__icon}>
+                  {page.icon}
+                </div>
+                <small className={NavbarStyles.navbar__menu__item__text}>
+                  {page.name}
+                </small>
+              </NavLink>
+            ) : (
+              <NavLink to={page.path}>
+                <div className={NavbarStyles.navbar__menu__item__icon}>
+                  {page.icon}
+                </div>
+                <small className={NavbarStyles.navbar__menu__item__text}>
+                  {page.name}
+                </small>
+              </NavLink>
+            )}
           </div>
         ))}
       </div>
-      <div className={NavbarStyles.extra__menu}>
+      <div className={NavbarStyles[toggleClass]}>
         {EXTRA_MENU.map((page, index) => (
           <div className={NavbarStyles.navbar__menu__item} key={index}>
             <NavLink to={page.path}>
